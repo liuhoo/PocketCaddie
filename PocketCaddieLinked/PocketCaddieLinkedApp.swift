889e35610451cd7a2d7f39e2ef2962b9270ed9ed
Alex Liu <alexliulxl@gmail.com>
2023-05-30 11:53:07 -0700

    Initial Commit

---
+++ PocketCaddieLinked/PocketCaddieLinkedApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketCaddieLinkedApp.swift
+//  PocketCaddieLinked
+//
+//  Created by Alex Liu on 5/30/23.
+//
+
+import SwiftUI
+
+@main
+struct PocketCaddieLinkedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
